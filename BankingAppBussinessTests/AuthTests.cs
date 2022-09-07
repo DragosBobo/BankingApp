9b76fd2fa28057b0b12e51a78f9d61ab48708507
Dragos Boboluta <dragos.boboluta@nagarro.com>
2022-09-07 17:54:22 +0300

    Auth login test done

--- BankingAppBussinessTests/AuthTests.cs
+++ BankingAppBussinessTests/AuthTests.cs
@@ -8,7 +8,7 @@ using Microsoft.AspNetCore.Identity;
 using Moq;
 
 namespace BankingAppBussinessTests
-{  
+{
     [TestClass]
     public class AuthRepositoryTests : BaseTest
     {
@@ -19,18 +19,18 @@ namespace BankingAppBussinessTests
         private readonly Mock<IHttpContextAccessor> contextAccesor = new Mock<IHttpContextAccessor>();
         private readonly Mock<IUserClaimsPrincipalFactory<User>> claimFactory = new Mock<IUserClaimsPrincipalFactory<User>>();
         private readonly Mock<AuthRepository> auth2 = new Mock<AuthRepository>();
-        public AuthRepositoryTests( )
+        public AuthRepositoryTests()
         {
             context = Context();
-            userManager = new Mock<UserManager<User>>(store.Object,null,null,null,null, null, null, null, null);
-            userManager.Setup(x => x.CreateAsync(It.IsAny<User>(), It.IsAny<string>())).ReturnsAsync(IdentityResult.Success).Callback<User, string>((x, y) => { context.Add(x); context.SaveChanges(); }) ;
-            signInManager = new Mock<SignInManager<User>>(userManager.Object,contextAccesor.Object,claimFactory.Object,null,null,null);
+            userManager = new Mock<UserManager<User>>(store.Object, null, null, null, null, null, null, null, null);
+            userManager.Setup(x => x.CreateAsync(It.IsAny<User>(), It.IsAny<string>())).ReturnsAsync(IdentityResult.Success).Callback<User, string>((x, y) => { context.Add(x); context.SaveChanges(); });
+            signInManager = new Mock<SignInManager<User>>(userManager.Object, contextAccesor.Object, claimFactory.Object, null, null, null);
         }
         [TestMethod]
-       
+
         public async Task TestRegister()
         {   //Arange
-            var sut = new AuthRepository(context,userManager.Object,signInManager.Object);
+            var sut = new AuthRepository(context, userManager.Object, signInManager.Object);
             var user = new RegisterApiModel
             {
                 FirstName = "Popescu",
@@ -43,12 +43,38 @@ namespace BankingAppBussinessTests
 
             //Act
             await sut.Register(user);
-           
+
             //Assert
             context.Users.Should().HaveCount(1);
         }
         [TestMethod]
 
-      
+        public async Task TestLogin()
+        {
+            //Arange
+            var sut = new AuthRepository(context, userManager.Object, signInManager.Object);
+            var user = new RegisterApiModel
+            {
+                FirstName = "Popescu",
+                LastName = "Marian",
+                UserName = "PopescuMarian2000",
+                Email = "Popescu.Marian@Gmail.com",
+                Password = "Parola12345@",
+                ConfirmedPassword = "Parola12345@"
+            };
+            var loginModel = new LoginApiModel
+            {
+                Email = user.Email,
+                Password = user.Password,
+            };
+
+            //Act
+            await sut.Register(user);
+            var result = sut.Login(loginModel);
+
+            //Assert
+            result.Should().NotBeNull();
+        }
+
     }
 }
