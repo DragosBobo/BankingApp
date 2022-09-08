316ccb56007b87301c0d7cf57b00e65e236365b1
dragos boboluta <dragos.boboluta@nagarro.com>
2022-09-08 06:58:16 -0700

    test for geting all transactions

--- BankingAppBussinessTests/TransactionTests.cs
+++ BankingAppBussinessTests/TransactionTests.cs
@@ -1,6 +1,8 @@
+using BakingAppDataLayer;
 using BankingAppApiModels.Models.Requests;
 using BankingAppBusiness.TransactionRepo;
 using FluentAssertions;
+using CategoryTransaction = BankingAppApiModels.Models.Requests.CategoryTransaction;
 
 namespace BankingAppBussinessTests
 {
@@ -29,6 +31,37 @@ namespace BankingAppBussinessTests
             //Assert
             context.Transactions.Should().HaveCount(1);
             transaction.Should().BeEquivalentTo(foundTransaction, opt => opt.Excluding(x => x.Id).Excluding(x => x.Account).Excluding(x => x.AccountId).Excluding(x => x.Amount));
+        }
+        [TestMethod]
+        public async Task TestGetTransactions()
+        {
+            //Arrange 
+            var transRepo = new TransactionRepository(context);
+            var transactions = new List<Transaction>()
+            {
+                new Transaction()
+                {
+                    Amount = 200, 
+                    TransactionDate = DateTime.Now,
+                    AccountId = new Guid("a0214d14-5570-4970-9834-c86f321e594b"),
+                    CategoryTransaction = (BakingAppDataLayer.CategoryTransaction)CategoryTransaction.Entertainment
+                },
+                new Transaction()
+                {   
+                    Amount = 350,
+                    TransactionDate = DateTime.Now,
+                    AccountId = new Guid("cb254d14-5570-4323-9834-c86f3321594b"),
+                    CategoryTransaction = (BakingAppDataLayer.CategoryTransaction)CategoryTransaction.Food
+                }
+            };
+            context.Transactions.AddRange(transactions);
+            context.SaveChanges();
+
+            //Act
+            var result = await transRepo.GetTransactions();
+
+            //Assert
+            result.Should().HaveCount(transactions.Count);
         }
     }
 }
